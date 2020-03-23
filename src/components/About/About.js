cb9f6db0925d80deafc3463514afb948c862a526
Ben Duncan <benduncan@Bens-iMac.local>
2020-03-22 17:50:05 -1000

    made changes to the about page. social icons are now underneath the large text block seperated by a line. More content to come on this page.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -38,11 +38,18 @@ const About = () => {
                         research scientist
                 </motion.div>
             </div>
+            <motion.div className="horizontal-line"
+                animate={{ scaleX: [0, 1] }}
+                transition={{ delay: 3.25, duration: 0.5 }}
+            ></motion.div>
             <motion.div className="social-icons"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
+                // initial={{ opacity: 0 }}
+                // animate={{ opacity: 1 }}
+                animate={{ scaleX: [0, 1] }}
                 transition={{ delay: 3.25, duration: .5 }} >
-                    <img src={ twitter } alt="twitter icon" className="social" id="social1"></img>
+                    <a href="https://twitter.com/itsbenduncan" className="social" id="social1" target="_blank">
+                        <img src={ twitter } alt="twitter icon" className="social" id="social1"></img>
+                    </a>    
                     <img src={ github } alt="github icon" className="social" id="social2"></img>
                     <img src={ instagram } alt="instagram icon" className="social" id="social3"></img>
             </motion.div>
